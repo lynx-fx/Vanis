cdc795d03db907e88cda45098f7c21dca8690c11
lynx-fx <anupbhujel07@gmail.com>
2025-07-17 20:21:16 +0545

    Client initialization

---
+++ client/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold mb-8">Welcome to My Next.js App!</h1>
+        
+         </main>
+  )
+}
