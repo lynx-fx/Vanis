849feefdc11ffe1a7f0cc8a09b120e2f14414644
lynx-fx <anupbhujel07@gmail.com>
2025-07-19 21:08:12 +0545

    Landing page's skeleton

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold mb-8">Welcome to My Next.js App!</h1>
-        
-         </main>
-  )
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 caret-transparent">
+        <h1 className="text-bold text-4xl">Vanis</h1>
+        <div className="flex w-full h-full justify-around">
+          <div className="w-1/2 flex justify-center border-2 p-32">
+            <h2>Upload</h2>
+          </div>
+          <div className="w-1/2 flex justify-center border-2 p-32">
+            <h2>Download</h2>
+          </div>
+        </div>
+        <div >
+          <button>
+            Donate
+          </button>
+        </div>
+      </main>
+    </>
+  );
 }
